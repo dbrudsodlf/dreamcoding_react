f801b14da7e688462f57fc19d493d1552e7db11e
yukyung kim <contiki0527@naver.com>
2023-05-25 22:28:33 +0900

    reducer

---
+++ src/AppWrap.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function AppWrap(){
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    );
+}
+
+function Navbar(){
+    return(
+        <header>
+            <Avatar>
+                
+            </Avatar>
+        </header>
+    )
+}
