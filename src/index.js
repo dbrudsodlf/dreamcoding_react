0b835a3eb0b1224bf8c6f9a65968bcc71a2bd1ba
yukyung kim <contiki0527@naver.com>
2023-05-31 21:47:50 +0900

    error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import App from './AppForm';
+import App from './basic/AppProducts';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
